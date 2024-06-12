417421c5cc2030f1d4e15c9068461e825cffd53a
Vodka Ng <vodka@devx.com.au>
2024-06-12 17:42:26 +0700

    fix bugs

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./better-nestjs-zod-prisma.cjs.production.min.js')
-} else {
-  module.exports = require('./better-nestjs-zod-prisma.cjs.development.js')
-}
